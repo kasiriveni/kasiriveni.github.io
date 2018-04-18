0bd1053325f9e4534d6d2a7297f9944cc6b99340
Srinivas Kasiriveni <kasiriveni@gmail.com>
2018-04-18 16:09:18 +0530

    testing the Live url
    need to fine more

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path=require('path');
-const webpack = require('webpack'); 
+const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports={
   entry:['babel-polyfill','./src/js/index.js'],
   output:{
-    path:path.resolve(__dirname,'dist'),
+    path:path.resolve(__dirname,'./'),
     filename:'bundle.js'
   },
   module: {
     rules: [
-      { test: /\.js$/, 
-        exclude: /node_modules/, 
-        loader: "babel-loader" 
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
       },
       {
         test: /\.css$/,
