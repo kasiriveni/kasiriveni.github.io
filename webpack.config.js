b65379b4f9932aa62d40d6c1c7fb45ec2dfb51f5
Srinivas Kasiriveni <kasiriveni@gmail.com>
2018-04-18 15:31:16 +0530

    webpack setup
    improve the webpack build process🤔

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,26 @@
 const path=require('path');
+const webpack = require('webpack'); 
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 module.exports={
-    entry:['babel-polyfill','./src/js/index.js'],
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'bundle.js'
-    },
-    module: {
-        rules: [
-          { test: /\.js$/, 
-            exclude: /node_modules/, 
-            loader: "babel-loader" 
-          },
-          {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-          }
-        ]
+  entry:['babel-polyfill','./src/js/index.js'],
+  output:{
+    path:path.resolve(__dirname,'dist'),
+    filename:'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: "babel-loader" 
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
       }
+    ]
+  },
+  plugins:[
+    new CleanWebpackPlugin('dist')
+  ]
 }
