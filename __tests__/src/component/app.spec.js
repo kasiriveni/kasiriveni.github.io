b7c6b718c11bbda52bc431f10acb6faf4158ce5a
Srinivas Kasiriveni <kasiriveni@gmail.com>
2018-04-24 11:44:20 +0530

    ading the jest
    ``` jest support```

---
+++ __tests__/src/component/app.spec.js
@@ -0,0 +1,15 @@
+import {sum} from '../../../src/js/app'
+
+test('adds 1 + 2 to equal 3', () => {
+	expect(sum(1, 2)).toBe(3);
+  });
+
+  test('two plus two is four', () => {
+	expect(2 + 2).toBe(4);
+  });
+
+  test('object assignment', () => {
+	const data = {one: 1};
+	data['two'] = 2;
+	expect(data).toEqual({one: 1, two: 2});
+  });
